80ab5a46b103eb4a3bd55ef75aa26d8786059590
lou2y <unse06.i@gmail.com>
2024-05-24 16:17:17 +0900

    0524

--- Assets/Script/SelectCharacter.cs
+++ Assets/Script/SelectCharacter.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -12,22 +15,66 @@ public class NewBehaviourScript : MonoBehaviour
 
     [Header("Character")]
     public GameObject[] Characters;
+    public CharacterIno[] characterInfos;
     private int charIndex = 0;
 
+    [Header("GameStart")]
+    public GameObject GameStart;
+    public Text GameCountTxt;
+    public bool isPlayButtonCliked = false;
+    private float gameCount = 5f;
+
+    public static string CharacterName;
+
+    private void Update()
+    {
+        if (isPlayButtonCliked)
+        {
+            gameCount -= Time.deltaTime;
+            if (gameCount <= 0)
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+            GameCountTxt.text = $"곧 게임이 시작됩니다/ \n {gameCount:F1}";
+        }
+    }
+
+    public void PlayBtn()
+    {
+        GameStart.SetActive(true);
+        isPlayButtonCliked = true;
+        CharacterName = Characters[charIndex].name;
+    }
     public void SelectCharacterBtn(string btnName)
     {
-        if(btnName == "Next")
+        Characters[charIndex].SetActive(false);
+        if (btnName == "Next")
         {
             charIndex++;
             charIndex = charIndex % Characters.Length;
         }
-        if(btnName == "Prev")
+        if (btnName == "Prev")
         {
             charIndex--;
             charIndex = charIndex % Characters.Length;
             charIndex = charIndex < 0 ? charIndex + Characters.Length : charIndex;
         }
         Debug.Log($"CharIndex : {charIndex}");
+        Characters[charIndex].SetActive(true);
+        SetPanelInfo();
     }
+
+    private void SetPanelInfo()
+    {
+        NameTxt.text = characterInfos[charIndex].Name;
+        FeatureTxt.text = characterInfos[charIndex].Feature;
+        CharImage.sprite = Characters[charIndex].GetComponent<SpriteRenderer>().sprite;
+    }
+
+    private void Start()
+    {
+        SetPanelInfo();
+    }
+
 }
 
